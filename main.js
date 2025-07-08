b0b32e10f7ea8d10e4a3a2da3956cdeab9f6e65d
AnhTung-it-03 <Pyndaica000@gmail,com>
2025-07-08 11:12:30 +0700

    l1

---
+++ main.js
@@ -0,0 +1,8 @@
+let feet = prompt("nhập feet - meters");
+let meter = prompt("nhập meters - feet");
+
+let output_1 = 0.305 * feet  ;
+let output_2 = 3.279 * meter ;
+
+document.writeln("feet -> meters = "+ output_1);
+document.writeln("meters -> feet = "+ output_2);
